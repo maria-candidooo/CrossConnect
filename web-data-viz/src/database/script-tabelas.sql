5f324864650cd29828470a891fa3c8840f6a1e77
maria-candidooo <maria.candido@sptech.school>
2025-05-19 19:10:41 -0300

    Separando o Header em um arquivo separado

--- web-data-viz/src/database/script-tabelas.sql
+++ web-data-viz/src/database/script-tabelas.sql
@@ -1,62 +1,42 @@
--- Arquivo de apoio, caso você queira criar tabelas como as aqui criadas para a API funcionar.
--- Você precisa executar os comandos no banco de dados para criar as tabelas,
--- ter este arquivo aqui não significa que a tabela em seu BD estará como abaixo!
+create database crossconnect;
 
-/*
-comandos para mysql server
-*/
+use crossconnect;
 
-CREATE DATABASE aquatech;
+create table usuario (
+  idusuario int  primary key auto_increment,
+  nome varchar(45),
+  sobrenome varchar(45),
+  email varchar(200),
+  senha varchar(100));
 
-USE aquatech;
-
-CREATE TABLE empresa (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	razao_social VARCHAR(50),
-	cnpj CHAR(14),
-	codigo_ativacao VARCHAR(50)
+create table post (
+  idpost int  primary key auto_increment,
+  idusuario int not null,
+  conteudo varchar(600),
+  imagem text,
+  categoria enum('Dúvidas', 'Testemunhos', 'Pedidos de Oração', 'Eventos', 'Outros'),
+  status enum('Ativo', 'Inativo', 'Em análise') default 'Ativo',
+  data datetime,
+  constraint fk_post_usuario foreign key (idusuario) references usuario (idusuario)
 );
 
-CREATE TABLE usuario (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	nome VARCHAR(50),
-	email VARCHAR(50),
-	senha VARCHAR(50),
-	fk_empresa INT,
-	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
+create table denuncia (
+  iddenuncia int primary key  auto_increment,
+  idusuario int not null,
+  descricao varchar(50) not null,
+  idpost int not null,
+  status enum('Pendente', 'Avaliada', 'Ignorada') default 'Pendente',
+  constraint fk_denuncia_usuario foreign key (idusuario) references usuario (idusuario),
+  constraint fk_denuncia_post foreign key (idpost) references post (idpost)
 );
 
-CREATE TABLE aviso (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	titulo VARCHAR(100),
-	descricao VARCHAR(150),
-	fk_usuario INT,
-	FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
+create table favoritos (
+  idfavorito int primary key auto_increment,
+  idpost int not null,
+  idusuario int not null,
+  unique key (idpost, idusuario), -- para evitar que tenha favoritos repetidos
+  constraint fk_favoritos_post foreign key (idpost) references post (idpost),
+  constraint fk_favoritos_usuario foreign key (idusuario) references usuario (idusuario)
 );
+select * from usuario;
 
-create table aquario (
-/* em nossa regra de negócio, um aquario tem apenas um sensor */
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	descricao VARCHAR(300),
-	fk_empresa INT,
-	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
-);
-
-/* esta tabela deve estar de acordo com o que está em INSERT de sua API do arduino - dat-acqu-ino */
-
-create table medida (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	dht11_umidade DECIMAL,
-	dht11_temperatura DECIMAL,
-	luminosidade DECIMAL,
-	lm35_temperatura DECIMAL,
-	chave TINYINT,
-	momento DATETIME,
-	fk_aquario INT,
-	FOREIGN KEY (fk_aquario) REFERENCES aquario(id)
-);
-
-insert into empresa (razao_social, codigo_ativacao) values ('Empresa 1', 'ED145B');
-insert into empresa (razao_social, codigo_ativacao) values ('Empresa 2', 'A1B2C3');
-insert into aquario (descricao, fk_empresa) values ('Aquário de Estrela-do-mar', 1);
-insert into aquario (descricao, fk_empresa) values ('Aquário de Peixe-dourado', 2);
