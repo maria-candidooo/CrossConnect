f3bd6d10e695de8008d6d21462890313ad131e2d
maria-candidooo <maria.candido@sptech.school>
2025-06-06 12:10:13 -0300

    Alterando css

--- web-data-viz/src/models/favoritosModel.js
+++ web-data-viz/src/models/favoritosModel.js
@@ -1,31 +1,24 @@
 const database = require("../database/config");
 
-// Adicionar favorito
+// favoritar
 function favoritos(req, res) {
     const { idusuario, idpost } = req.body;
-    console.log("Favoritar:", idusuario, idpost)
+   
     if (!idusuario || !idpost) {
         return res.status(400).json({ erro: "idusuario e idpost são obrigatórios" });
     }
     const instrucaoSql = `
-        INSERT INTO favoritos (idusuario, idpost)
-        VALUES (${idusuario}, ${idpost});
+        insert into favoritos (idusuario, idpost)
+        values (${idusuario}, ${idpost});
     `;
     database.executar(instrucaoSql)
         .then(resultado => res.status(201).json(resultado))
-        .catch(erro => {
-            if (erro.code === 'ER_DUP_ENTRY') { // MySQL
-                res.status(200).json({ mensagem: "Já favoritado" });
-            } else {
-                res.status(500).json(erro);
-            }
-        });
+       
 }
 
-// Remover favorito
+// remover favorito
 function deletar(req, res) {
     const { idpost, idusuario } = req.params;
-    console.log("Desfavoritar:", idusuario, idpost);
 
     const instrucaoSql = `
         DELETE FROM favoritos WHERE idpost = ${idpost} AND idusuario = ${idusuario};
@@ -35,7 +28,7 @@ function deletar(req, res) {
         .catch(erro => res.status(500).json(erro));
 }
 
-// Listando os  favoritos do usuário
+// listando os  favoritos do usuário
 function listarPorUsuario(req, res) {
     const { idusuario } = req.params;
     const instrucaoSql = `
